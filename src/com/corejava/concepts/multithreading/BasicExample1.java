57431bb905de14967137666377b02fdda7dac338
Sourabh Kumar <sourabh.kumar@e2open.com>
2025-06-02 20:50:59 +0530

    Commit Multithreading

--- src/com/corejava/concepts/multithreading/BasicExample1.java
+++ src/com/corejava/concepts/multithreading/BasicExample1.java
@@ -6,7 +6,7 @@ class MathUtils {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(number + " * " + i + " = " + (number * i));
 			try {
-				Thread.sleep(500);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				System.out.println("Thread interrupted: " + e.getMessage());
 			}
@@ -46,11 +46,13 @@ public class BasicExample1 {
 	public static void main(String[] args) {
 		MathUtils mathUtils = new MathUtils();
 
+		// Creating threads by extending Thread class which are using common resource i.e. mathUtils
 		Thread thread1 = new TableFiveThread(mathUtils);
 		Thread thread2 = new TableSevenThread(mathUtils);
 		thread1.start();
 		thread2.start();
-
+ 
+		// Creating threads using anonymous inner classes which are using common resource i.e. mathUtils
 		Thread thread3 = new Thread() {
 			@Override
 			public void run() {
@@ -67,6 +69,18 @@ public class BasicExample1 {
 
 		thread3.start();
 		thread4.start();
+
+		// Creating a thread using a lambda expression which is using common resource i.e. mathUtils
+		Thread thread5 = new Thread(() -> {
+			mathUtils.createTable(15);
+		});
+
+		Thread thread6 = new Thread(() -> {
+			mathUtils.createTable(20);
+		});
+
+		thread5.start();
+		thread6.start();
 
 		System.out.println("Thread started: " + Thread.currentThread().getName());
 	}
